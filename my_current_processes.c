7958a739bab99d3124385e61dc0d5bdff584291c
JLJinhanLiu <jinhan.liu@ontariotechu.net>
2021-01-19 17:46:59 -0500

    Question 1 Finished

--- my_current_processes.c
+++ my_current_processes.c
@@ -1,32 +1,63 @@
 #include <stdio.h>
 #include <dirent.h>
 #include <string.h>
+#include <ctype.h>
+#include <stdlib.h>
 
-void 
+const char topPath[] = "/proc/";
 
-int listdir(const char *path) 
-{
-  struct dirent *entry;
-  DIR *dp;
+void printProcesses(struct dirent *dirItem){
 
-  dp = opendir(path);
-  if (dp == NULL) 
-  {
-    perror("opendir");
-    return -1;
+  //print PID and tab
+  printf("%s\t", dirItem->d_name);
+
+  //grab path to cmdline file 
+  char *filePath = malloc(100);
+  strcat(filePath, topPath);
+  strcat(filePath, dirItem->d_name);
+  strcat(filePath, "/cmdline");
+
+  FILE *cmdlineFile = fopen(filePath, "r");
+  if (cmdlineFile == NULL){
+    perror("cmdline file not found for a process");
+    exit -1;
+  }
+  
+  //grab first line of cmdline file, i.e. the process name
+  char *processName = malloc(100);
+  fgets(processName, 100, cmdlineFile);
+  printf("%s\n", processName);
+  
+  fclose(cmdlineFile);
+  free(filePath);
+}
+
+int printDirectory() {
+
+  struct dirent *dirItem;
+  DIR *directory = opendir(topPath);
+
+  //check if directory can be opened
+  if (directory == NULL) {
+      perror("/proc directory not found");
+      return -1;
   }
 
-  while((entry = readdir(dp)))
-    puts(entry->d_name);
+  while(dirItem = readdir(directory)){
+  
+      //check if entry is of type 4 (folder) and if name is numerical
+      if ((dirItem->d_type == 4) && (isdigit(*(dirItem->d_name)))) 
+          printProcesses(dirItem);
 
-  closedir(dp);
+  }
+
+  closedir(directory);
   return 0;
 }
 
 int main() {
-
     
-    listdir("/proc");
+    printDirectory();
 
     return 0;
 }
