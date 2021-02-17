fbe893a0ac0629164e53b1e14d06dbf4aa9d364d
JLJinhanLiu <zthlm7271@gmail.com>
2021-02-17 01:00:02 -0500

    Added function to change themes, still WIP. WIP new printing mechanism that supports colours.

--- cshell.c
+++ cshell.c
@@ -10,11 +10,39 @@ typedef struct{
 }EnvVar;
 
 typedef struct{
-        char *name;
-        struct tm time;
+    char *name;
+    struct tm time;
     int code;
 }Command;
 
+/*
+prints the string with the proper colour formatting
+colour can be redefined by user. Default is red.
+*/
+void shell_print(int colour, const char* STRING){
+    printf("\033[0;%im%s\033[0;37m", colour, STRING);
+}
+
+int change_theme(const char* NEW_COLOUR, int old_colour){
+    if (strcmp(NEW_COLOUR, "red") == 0)
+        return 31;
+    else if (strcmp(NEW_COLOUR, "green") == 0) 
+        return 32;
+    else if (strcmp(NEW_COLOUR, "orange") == 0)
+        return 33;
+    else if (strcmp(NEW_COLOUR, "blue") == 0)
+        return 34;
+    else if (strcmp(NEW_COLOUR, "purple") == 0)
+        return 35;
+    else if (strcmp(NEW_COLOUR, "cyan") == 0)
+        return 36;
+    else if (strcmp(NEW_COLOUR, "gray") == 0)
+        return 37;
+    else{
+        shell_print(old_colour, "Unknown theme.\n");
+        return old_colour;
+    }
+}
 
 
 int main(){
@@ -25,15 +53,20 @@ int main(){
     EnvVar EnvVariables[255]; // enviroment variables array, currently set to 255
     int enviromentCounter = 0; // currently setting a global counter, we could readjust so that it initializes with NULL values and find the first null value to edit
 
-
+    int shell_colour = 31; //default red theme colour value
 
     while(strcmp(input, "exit") != 10){ // For some reason strcmp is returning 10 if they are equal not 0???? 
+        
         int counter = 0; // for indexing array
+
+        shell_print(shell_colour, "cshell$ ");
         fgets(input, sizeof(input), stdin);
+
         arguments[counter] = strtok(input, delimeter); // get first argument
         while(arguments[counter] != NULL ){
             arguments[++counter] = strtok(NULL, delimeter); // get the rest of the arguments
         }
+        
         /**check arguments[0]:
          * If is starts with $ create a new enviroment variable
          * If its log print the Command array
@@ -41,6 +74,7 @@ int main(){
          * If it starts with theme take argument[1] and change the prompt colour
          * otherwise call fork() on argument[0] and pass arguments[1] through arguments[n]
          * */
+        
         strcpy(command, arguments[0]); // get the first argument
         if(command[0] == '$'){
             char *name = strtok(command, "="); // parse the name and value, push into array EnvVariables
@@ -50,7 +84,7 @@ int main(){
             temp.name = name;
             temp.value = value;
             EnvVariables[enviromentCounter++] = temp;
-        }else if(strcmp(&command, "print") == 0){
+        }else if(strcmp(command, "print") == 0){
             char *token;
             for(int i = 1; i < counter; i++){
                 token = arguments[i];
@@ -66,13 +100,14 @@ int main(){
                     printf("%s \n", arguments[i]);
                 }
             }
+        }else if(strcmp(command, "theme") == 0){
+            shell_colour = change_theme(arguments[1], shell_colour);
         }
         else {
             printf("Current command is %s %d", command);
         }
 
-        
     }
-    printf("goodbye");
+    shell_print(shell_colour, "goodbye");
     return 0;
 }
