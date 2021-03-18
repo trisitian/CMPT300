1b3513a528cf5a751d94017942fe41df3ab1c23f
JLJinhanLiu <zthlm7271@gmail.com>
2021-03-18 19:49:26 -0400

    trying to implement working port &  ip input into threads

--- lets-talk.c
+++ lets-talk.c
@@ -10,13 +10,13 @@
 List *senderList, *recieverList;
 // struct to carry variables into threads
 struct threadArg{
-    int *sockfd;                    // socket file descriptor
-    struct sockaddr_in *socketInOut;// socket struct
+    long ip;
+    long portIN;
+    long portOUT;
 };
 
-
 // Will cause seg fault if user enters incorrect IP
-int IPtoInt(char* normalForm){
+long IPtoInt(char* normalForm){
     long first, second, third, fourth;
     const char delimeter[2] = ".";
     first = pow(256,3)*atoi(strtok(normalForm, delimeter));
@@ -24,7 +24,6 @@ int IPtoInt(char* normalForm){
     third = 256*atoi(strtok(NULL, delimeter));
     fourth = atoi(strtok(NULL, delimeter));
     return first + second + third+ fourth;
-
 }
 
 /**
@@ -77,7 +76,7 @@ void *awaitInput(void *ptr){
     return 0;
 }
 // Thread for receiving messages
-void *receivingThread(struct threadArg *threadArgs){
+void *receivingThread(struct threadArgs *threadArguments){
 
     // create socket
     int sockfd;
@@ -90,7 +89,7 @@ void *receivingThread(struct threadArg *threadArgs){
     struct sockaddr_in receiver, source;
     bzero(&receiver, sizeof(receiver));
     receiver.sin_family = AF_INET;
-    receiver.sin_addr.s_addr = htonl(2130706433); // htonl(ip); // uncooment functions to use actual IPs
+    receiver.sin_addr.s_addr = htonl(threadArguments.ip); // htonl(ip); // uncooment functions to use actual IPs
     receiver.sin_port = htons(6000); // htons(port);
 
     // bind socket to socket address
@@ -112,30 +111,14 @@ void *receivingThread(struct threadArg *threadArgs){
         //add lock here
         List_add(recieverList, buffer);
         // add unlock here
-        printf("%s", buffer);
-        close(sockfd);
-        break;
-
     }
-    // DEPRECATED
-//     close(threadArgs->fds[0]);
-//     char buffer[4000];
-//     struct sockaddr_in addressSource;
-//     int sourceLength = sizeof(addressSource);
-//     while (1){
-//         // receive messages
-//         if ( recvfrom(*(threadArgs->sockfd), buffer, 4000, 0, (struct sockaddr*) &addressSource, (unsigned int *)&sourceLength) < 0){
-//             perror("Error receiving message");
-//             exit(EXIT_FAILURE);
-//         };
-//         write(threadArgs->fds[1], buffer, 4000);
-//     }
-// }
+    close(sockfd);
+
     return 0;
 }
 
 // Thread for sending messages
-void *sendingThread(struct threadArg *threadArgs){
+void *sendingThread(struct threadArgs *threadArguments){
 
     // create socket
     int sockfd;
@@ -147,7 +130,7 @@ void *sendingThread(struct threadArg *threadArgs){
     // assign values to socket address
     struct sockaddr_in receiver;
     receiver.sin_family = AF_INET;
-    receiver.sin_addr.s_addr = htonl(2130706433); // htonl(ip); // uncooment functions to use actual IPs
+    receiver.sin_addr.s_addr = htonl(*(long *)threadArguments.ip); // htonl(ip); // uncooment functions to use actual IPs
     receiver.sin_port = htons(6000); // htons(port);    
 
     int sourceLen = sizeof(struct sockaddr_in);
@@ -168,19 +151,7 @@ void *sendingThread(struct threadArg *threadArgs){
             }
         }
     }
-
     close(sockfd);
-    // DEPRECATED
-    // struct sockaddr_in addressTo;
-    // int sourceLength = sizeof(addressTo);
-    
-    // while (1){
-    //     // send messages
-    //     if ( sendto(*(threadArgs->sockfd), "Hello!", sizeof("Hello!"), 0, (struct sockaddr*) &addressTo, sourceLength) < 0 ){
-    //         perror("Error sending message");
-    //         exit(EXIT_FAILURE);
-    //     }
-    // }
     return 0;
 }
 
@@ -205,42 +176,22 @@ int main(int argc, char* argv[]){
         exit(EXIT_FAILURE);
     }
 
+    // create lists for conversations
     senderList = List_create();
     recieverList = List_create();
+
+    // for passing variables into threads
+    struct threadArg threadArguments;               
+    threadArguments.ip = IPtoInt(argv);
     
     pthread_t keyboardIn, UDPOut, UDPIn, screenOut; // threads to handle keyboard, UDP, and output
-    struct threadArg threadArguments;               // for passing variables into threads
-    int remoteClientIP, localPort, remotePort;
-    localPort = atoi(argv[1]);
+    threadArguments.portIN = atoi(argv[1]);
     if(strcmp(argv[2], "localhost") == 0){
-        remoteClientIP = 2130706433; // 127.0.0.1 decimal 
+        threadArguments.ip = 2130706433; // 127.0.0.1 decimal 
     }else{
-        remoteClientIP = IPtoInt(argv[2]);
+        threadArguments.ip = IPtoInt(argv[2]);
     }
-    remotePort = atoi(argv[3]);
-
-    //printf("%s \n", test);
-
-    // // init sockets
-    // int sockfd;
-    // if ( (sockfd = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)) < 0){ // IPv4, UDP, default protocal
-    //     perror("Failed to create socket");
-    //     exit(EXIT_FAILURE);
-    // } 
-
-    // struct sockaddr_in addressOut;
-    // addressOut.sin_family = AF_INET;
-    // addressOut.sin_addr.s_addr = htonl(2130706433); // htonl(ip); // uncooment functions to use actual IPs
-    // addressOut.sin_port = htons(6000); // htons(port);
-
-    // // bind socket to struct
-    // if ( bind(sockfd, (const struct sockaddr*) &addressOut, sizeof(addressOut)) < 0 ){
-    //     perror("Failed to bind socket");
-    //     exit(EXIT_FAILURE);
-    // }
-
-    // threadArguments.socketInOut = &addressOut;
-    // threadArguments.sockfd = &sockfd;
+    threadArguments.portOUT = atoi(argv[3]);
 
     //initialize threads
     pthread_create(&keyboardIn, NULL, awaitInput, NULL);
