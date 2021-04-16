0bb6aac01646bac7722dbb4873ea13eaa2b8be7f
JLJinhanLiu <zthlm7271@gmail.com>
2021-04-16 15:08:28 -0400

    refactor, multiple flags working

--- myls.c
+++ myls.c
@@ -20,66 +20,76 @@ struct Files{
 
 struct Files files;
 
-static void printFiles(int options){
+static void printFiles(){
     DIR *curr;
     struct dirent *dp;
+    struct stat info;
 
-    // open directory
-    curr = opendir(".");
+    // for each custom path it will loop back
+    // executing at least once for '.'
+    do{
+        // open directory
+        if (files.size == 0){
+            curr = opendir(".");
+
+        } else {
+            curr = opendir(files.FileList[files.size - 1]);
+        }
+
         if((curr == NULL)){
-            perror("Error opening directory \n");
-            exit(1);
-        }
-
-    if(options == 0){ // no args 
-        while((dp = readdir(curr)) != NULL){
-            printf("%s \n", dp->d_name);
-        }
-
-    }else if(options ==1){ // -i
-        while((dp = readdir(curr)) != NULL){
-            printf("\t%ld \t%s \n", dp->d_ino, dp->d_name); // her example starts with a leading tab
-        }
-
-    }else if(options == 2){ // -l
-        struct stat info;
-        while((dp = readdir(curr))!= NULL){
-            if(!stat(dp->d_name, &info)){
-                struct passwd *user = getpwuid(info.st_uid);
-                struct group *group = getgrgid(info.st_gid);
-                printf( (S_ISDIR(info.st_mode)) ? "d" : "-");
-                printf( (info.st_mode & S_IRUSR) ? "r" : "-");
-                printf( (info.st_mode & S_IWUSR) ? "w" : "-");
-                printf( (info.st_mode & S_IXUSR) ? "x" : "-");
-                printf( (info.st_mode & S_IRGRP) ? "r" : "-");
-                printf( (info.st_mode & S_IWGRP) ? "w" : "-");
-                printf( (info.st_mode & S_IXGRP) ? "x" : "-");
-                printf( (info.st_mode & S_IROTH) ? "r" : "-");
-                printf( (info.st_mode & S_IWOTH) ? "w" : "-");
-                printf( (info.st_mode & S_IXOTH) ? "x" : "-");
-                printf("\t%ld", info.st_nlink);
-                printf("\t%s", user->pw_name);
-                printf("\t%s", group->gr_name);
-                printf("\t%ld", info.st_size);
-                //printf("\t%s", info.st_atim); // not sure how to use this param
-                printf("\t%s\n",dp->d_name);
+                printf("Error opening directory %s", files.FileList[files.size - 1]);
+                exit(1);
             }
-        }
 
-    }else if(options ==3){
-        printf("-R called");
-
-    }else if(options ==4){
-        char *temp;
-        int search = 0;
-        while(search <= files.size){
-            temp = files.FileList[search];
-            while((dp = readdir(curr))!= NULL){
-                // compare each file
+        // DEPRECATED? Maybe move into -R?
+        // if(files.size > 0){
+        //     char *temp;
+        //     int search = 0;
+        //     while(search <= files.size){
+        //         temp = files.FileList[search];
+        //         while((dp = readdir(curr))!= NULL){
+        //             // compare each file
+        //     }
+        // }
+        
+        // read directory
+        while((dp = readdir(curr)) != NULL){
+            if(flagBoolList[0]){ // -i
+                printf("%ld  ", dp->d_ino);
             }
+        
+            if(flagBoolList[1]){ // -l
+                if(!stat(dp->d_name, &info)){
+                    struct passwd *user = getpwuid(info.st_uid);
+                    struct group *group = getgrgid(info.st_gid);
+                    printf( (S_ISDIR(info.st_mode)) ? "d" : "-");
+                    printf( (info.st_mode & S_IRUSR) ? "r" : "-");
+                    printf( (info.st_mode & S_IWUSR) ? "w" : "-");
+                    printf( (info.st_mode & S_IXUSR) ? "x" : "-");
+                    printf( (info.st_mode & S_IRGRP) ? "r" : "-");
+                    printf( (info.st_mode & S_IWGRP) ? "w" : "-");
+                    printf( (info.st_mode & S_IXGRP) ? "x" : "-");
+                    printf( (info.st_mode & S_IROTH) ? "r" : "-");
+                    printf( (info.st_mode & S_IWOTH) ? "w" : "-");
+                    printf( (info.st_mode & S_IXOTH) ? "x" : "-");
+                    printf("  %ld", info.st_nlink);
+                    printf("  %s", user->pw_name);
+                    printf("  %s", group->gr_name);
+                    printf("  %ld  ", info.st_size);
+                    //printf("\t%s", info.st_atim); // not sure how to use this param
+                }
+            }
+
+            if(flagBoolList[2]){ // -R
+                printf("  -R called, but feature not implemented.  ");
+            }
+            
+            printf("%s\n", dp->d_name);
         }
-    }
-    closedir(curr);
+
+        closedir(curr);
+        files.size--;
+    } while (files.size > 0);
 }
 
 static void flagCheck(int location, char flagChar){
@@ -127,25 +137,7 @@ int main(int argc, char**argv){
         }
     }
 
-    if(files.size == 0 && flagCount == 0){ // call script on just current folder
-        printFiles(0);
+    printFiles();
 
-    // ***************ELSE IF CAUSING NOT PRINTING STACKED ARGUMENTS*********************************
-    }else if(flagBoolList[0]){
-        printFiles(1);
-
-    }else if(flagBoolList[1]){
-        printFiles(2);
-
-    }else if(flagBoolList[3]){
-        printFiles(3);
-
-    }else if(files.size > 0){
-        printFiles(4);
-
-    }else{
-        printf("Unknown error.\n");
-        exit(1);
-    }
     return 0;
 }
